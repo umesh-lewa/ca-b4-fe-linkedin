01de814262fd56854d18ed7a12a101fc37ae4a48
Vijay Akash <vjbakash@gmail.com>
2021-06-14 19:34:19 +0530

    profile title bar changed

--- linkedin-clone/src/app/pages/profile/profile.component.ts
+++ linkedin-clone/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { DataService } from 'src/app/services/data.service';
 import { MainService } from 'src/app/services/main.service';
 
@@ -10,7 +11,10 @@ import { MainService } from 'src/app/services/main.service';
 export class ProfileComponent implements OnInit {
   peopleList=[];
   peopleSuggestion=[];
-  constructor(private dataService:DataService) {
+  constructor(private dataService:DataService,private mainServ:MainService,private titleServ:Title) {
+    this.mainServ.changeActiveTitle("profile");
+    this.dataService.getUser().subscribe(data=>this.titleServ.setTitle(`${data.username} | LinkedIn`))
+    
     this.dataService.getPeoples().subscribe(data=>{
       this.peopleList=data.peoples.slice(0,5);
       this.peopleSuggestion=data.peoples.slice(5,10);
